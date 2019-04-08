76e62b38c2a6ef63d0f1f73650ce90898dc8d892
green sun <liao962381394@sina.cn>
2019-04-08 18:02:29 +0800

    更新ui

--- app/src/main/java/com/lyj/fakepivix/module/home/home/HomeFragment.kt
+++ app/src/main/java/com/lyj/fakepivix/module/home/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.lyj.fakepivix.module.home.home
 
 import android.os.Bundle
+import com.flyco.tablayout.listener.CustomTabEntity
 import com.lyj.fakepivix.R
 import com.lyj.fakepivix.app.base.BaseViewModel
 import com.lyj.fakepivix.app.base.FragmentationFragment;
+import com.lyj.fakepivix.app.entity.TabBean
 import com.lyj.fakepivix.databinding.FragmentHomeBinding
 
 
@@ -24,7 +26,12 @@ class HomeFragment : FragmentationFragment<FragmentHomeBinding, BaseViewModel<*>
 
 
     override fun init(savedInstanceState: Bundle?) {
-
+        val tabs = arrayListOf<CustomTabEntity>(
+                TabBean(title = getString(R.string.subtab_illust)),
+                TabBean(title = getString(R.string.subtab_comic)),
+                TabBean(title = getString(R.string.subtab_novel))
+                )
+        mBinding.tabLayout.setTabData(tabs)
     }
 
     override fun immersionBarEnabled(): Boolean = false
