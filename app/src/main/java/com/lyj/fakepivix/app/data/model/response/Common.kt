0a02c59a404e2a6f135883a42e225bca5600c383
greensun <liao962381394@sina.cn>
2019-04-09 23:15:44 +0800

    添加插画response

--- app/src/main/java/com/lyj/fakepivix/app/data/model/response/Common.kt
+++ app/src/main/java/com/lyj/fakepivix/app/data/model/response/Common.kt
@@ -9,8 +9,11 @@ package com.lyj.fakepivix.app.data.model.response
  */
 
 data class IllustListResp(
+    val contest_exists: Boolean = false,
     val illusts: List<Illust> = listOf(),
-    val next_url: Any = Any()
+    val next_url: String = "",
+    val privacy_policy: PrivacyPolicy = PrivacyPolicy(),
+    val ranking_illusts: List<RankingIllust> = listOf()
 )
 
 data class Illust(
@@ -64,4 +67,44 @@ data class ImageUrls(
     val large: String = "",
     val medium: String = "",
     val square_medium: String = ""
+)
+
+/**
+ * 插画排行榜
+ */
+data class RankingIllust(
+    val caption: String = "",
+    val create_date: String = "",
+    val height: Int = 0,
+    val id: Int = 0,
+    val image_urls: ImageUrls = ImageUrls(),
+    val is_bookmarked: Boolean = false,
+    val is_muted: Boolean = false,
+    val meta_pages: List<Any> = listOf(),
+    val meta_single_page: MetaSinglePage = MetaSinglePage(),
+    val page_count: Int = 0,
+    val restrict: Int = 0,
+    val sanity_level: Int = 0,
+    val series: Any = Any(),
+    val tags: List<TagX> = listOf(),
+    val title: String = "",
+    val tools: List<Any> = listOf(),
+    val total_bookmarks: Int = 0,
+    val total_view: Int = 0,
+    val type: String = "",
+    val user: User = User(),
+    val visible: Boolean = false,
+    val width: Int = 0,
+    val x_restrict: Int = 0
+)
+
+data class TagX(
+    val name: String = "",
+    val translated_name: Any = Any()
+)
+
+data class PrivacyPolicy(
+    val message: String = "",
+    val url: String = "",
+    val version: String = ""
 )
