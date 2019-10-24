72766f0b90332fa3cdf945d3fd667a98fd520560
liaoyijian <liao962381394@sina.cn>
2019-10-24 09:56:46 +0800

    添加buildSrc依赖

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -7,5 +7,23 @@
  */
 
 object Vers {
-    const val support_version = "28.0.0"
+    const val kotlin_version = "1.3.30"
+    const val gradle_version = "3.2.1"
+    const val support__version = "28.0.0"
+    const val rxJava_version = "2.2.4"
+    const val rxKt_version = "2.3.0"
+    const val coroutine_version = "1.3.0-M2"
+    const val autoDispose_version = "1.1.0"
+    const val retrofit_version = "2.6.0"
+    const val glide_version = "4.9.0"
+    const val gson_version = "2.8.5"
+    const val moshi_version = "1.8.0"
+    const val objectbox_version = "2.2.0"
+    const val butterKnife_version = "9.0.0-rc3"
+    const val fragmentation_version = "1.3.6"
+    const val tinker_version = "1.9.14.3"
+}
+
+fun implementationAndKapt() {
+
 }
